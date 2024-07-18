014ea4ef8ad6d577e4ecd5727ebf9908ecbdc31f
gabriel.lezo <gabriel.lezo@fiorilli.com.br>
2024-07-18 17:19:28 -0300

    Add compression header parsing (not yet impl)
    + code refactor

--- src/main/java/ClientProcessor.java
+++ src/main/java/ClientProcessor.java
@@ -13,7 +13,6 @@ import java.util.regex.Pattern;
 public class ClientProcessor implements Runnable {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
-
     private final Socket client;
 
     public ClientProcessor(Socket client) {
@@ -45,37 +44,31 @@ public class ClientProcessor implements Runnable {
         String version = requestLine[2];
 
         // headers
-        Map<String, String> headers = new HashMap<>();
-        String header;
-        while ((header = reader.readLine()) != null && !header.isBlank()) {
-            var arr = header.split(": ");
-            headers.put(arr[0], arr[1]);
-        }
+        Map<String, String> headers = parseHeaders(reader);
 
-        String response = "";
-        byte[] nonStringBody = new byte[0];
+        String responseHeader = "";
+        byte[] responseBody = new byte[0];
         Matcher matcher;
 
         if (target.equals("/")) {
-            response = "HTTP/1.1 200 OK\r\n\r\n";
+            responseHeader = "HTTP/1.1 200 OK\r\n";
         } else if ((matcher = Pattern.compile("/echo/(\\w*)").matcher(target)).matches()) {
-            String res = matcher.group(1);
-            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + res.length() + "\r\n\r\n" + res;
+            responseBody = matcher.group(1).getBytes();
+            responseHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n";
         } else if (target.equals("/user-agent")) {
-            String res = headers.get("User-Agent");
-            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + res.length() + "\r\n\r\n" + res;
+            responseBody = headers.get("User-Agent").getBytes();
+            responseHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n";
         } else if ((matcher = Pattern.compile("/files/(\\w+)").matcher(target)).matches()) {
             String fileName = matcher.group(1);
             if (method.equals("GET")) {
                 Path file = FileManager.getInstance().getFile(fileName);
                 if (!file.toFile().exists()) {
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    responseHeader = "HTTP/1.1 404 Not Found\r\n";
                 } else {
-                    response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + file.toFile().length() + "\r\n\r\n";
-                    nonStringBody = Files.readAllBytes(file);
+                    responseBody = Files.readAllBytes(file);
+                    responseHeader = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\n";
                 }
             } else if (method.equals("POST")) {
-
                 // read request body
                 char[] body = new char[Integer.parseInt(headers.get("Content-Length"))];
                 reader.read(body);
@@ -83,18 +76,39 @@ public class ClientProcessor implements Runnable {
                 Path file = FileManager.getInstance().overwriteFile(fileName);
                 if (Files.exists(file)) {
                     Files.writeString(file, new String(body));
-                    response = "HTTP/1.1 201 Created\r\n\r\n";
+                    responseHeader = "HTTP/1.1 201 Created\r\n";
                 }
             }
         } else {
-            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            responseHeader = "HTTP/1.1 404 Not Found\r\n";
         }
 
-        byte[] responseBytes = response.getBytes();
-        byte[] toReturn = new byte[responseBytes.length + nonStringBody.length];
+        // Compressing
+        if (CompressionScheme.isSupported(headers.get("Content-Encoding"))) {
+            responseHeader += "Content-Encoding: " + headers.get("Content-Encoding").trim().toLowerCase() + "\r\n";
+        }
+
+        if (responseBody.length > 0) {
+            responseHeader += "Content-Length: " + responseBody.length + "\r\n";
+        }
+
+        responseHeader += "\r\n";
+
+        byte[] responseBytes = responseHeader.getBytes();
+        byte[] toReturn = new byte[responseBytes.length + responseBody.length];
         System.arraycopy(responseBytes, 0, toReturn, 0, responseBytes.length);
-        System.arraycopy(nonStringBody, 0, toReturn, responseBytes.length, nonStringBody.length);
+        System.arraycopy(responseBody, 0, toReturn, responseBytes.length, responseBody.length);
 
         return toReturn;
+    }
+
+    private static Map<String, String> parseHeaders(BufferedReader reader) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        String header;
+        while ((header = reader.readLine()) != null && !header.isBlank()) {
+            var arr = header.split(": ");
+            headers.put(arr[0], arr[1]);
+        }
+        return headers;
     }
 }
---
+++ src/main/java/CompressionScheme.java
@@ -0,0 +1,13 @@
+public enum CompressionScheme {
+
+    GZIP;
+
+    public static boolean isSupported(String name) {
+        if (name == null || name.isBlank()) return false;
+        for (var val : CompressionScheme.values()) {
+            if (val.name().equalsIgnoreCase(name.trim())) return true;
+        }
+        return false;
+    }
+
+}
